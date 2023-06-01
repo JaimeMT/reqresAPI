de17b00564dfdd949858f6c2bf43a36a56018fbd
Jaime Mantas <mantasjaime@gmail.com>
2023-06-01 11:38:44 +0200

    Subida proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListUserComponent } from './components/list-user/list-user.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent,
+    ListUserComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
