d4aa771e26b883fb8cd42ef4416c5e0ed83d59f8
jesus <jesus.martinez.gonzalez1993@gmail.com>
2023-06-01 14:07:39 +0200

    terminado listado de users

--- src/app/components/user/list-user/list-user.component.ts
+++ src/app/components/user/list-user/list-user.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
 import { User } from 'src/app/interface/user.interface';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-list-user',
   templateUrl: './list-user.component.html',
-  styleUrls: ['./list-user.component.css']
+  styleUrls: ['./list-user.component.css'],
 })
 export class ListUserComponent {
-
-
   usuarios?: User[]; // puede ser nulo
 
-  constructor(private userService: UserService){
+  constructor(private userService: UserService) {
 
-    this.usuarios = this.userService.getUsuarios();
 
+  }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((res: any) => {
+      this.usuarios = res.data;
+    });
+  }
+
+  borrarUsuario(usuario:number){
+
+    this.userService.deleteUser(usuario);
+  }
 }
