b77f9b18cc9064dfbe6d5fb58e68149032b2fa49
jesus <jesus.martinez.gonzalez1993@gmail.com>
2023-06-01 14:07:28 +0200

    Terminado listado de resources

--- src/app/components/resource/list-resource/list-resource.component.ts
+++ src/app/components/resource/list-resource/list-resource.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Resource } from 'src/app/interface/resource.interface';
+import { ResourceService } from 'src/app/service/resource.service';
 
 @Component({
   selector: 'app-list-resource',
   templateUrl: './list-resource.component.html',
   styleUrls: ['./list-resource.component.css']
 })
-export class ListResourceComponent {
+export class ListResourceComponent implements OnInit {
+
+  recursos?: Resource[]; // puede ser nulo
+
+  constructor(private recursosService: ResourceService){
+
+
+
+
+}
+
+ ngOnInit(): void{
+
+  this.recursosService.getResources().subscribe((res : any) =>{
+
+    this.recursos = res.data;
+
+  })
+
+ }
 
 }
