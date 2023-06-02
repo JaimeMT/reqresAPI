5883d4e361dbf05a836e970b2988efbf45758dc4
Jose David <clavellino1993@gmail.com>
2023-06-02 11:47:21 +0200

    ARREGLO DEL NAVBAR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reqresAPI';
+  esPaginaPrincipal: boolean = false;
+
+  constructor(private router: Router) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.esPaginaPrincipal = (event.url === '/' || event.url === '/login' || event.url === '/register');
+      }
+    });
+  }
 }
